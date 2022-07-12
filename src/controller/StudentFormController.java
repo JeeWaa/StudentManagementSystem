1ec95349447c72eca204f279db0a47fc586f5a9f
jeewantha1234 <jeewanthafernando925@gmail.com>
2022-07-12 11:34:27 +0530

    controller

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtNIC;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colNIC;
+
+    public void saveStudent(ActionEvent actionEvent) {
+    }
+
+    public void updateStudent(ActionEvent actionEvent) {
+    }
+
+    public void deleteStudent(ActionEvent actionEvent) {
+    }
+}
---
+++ src/views/table/StudentTable.java
@@ -0,0 +1,4 @@
+package views.table;
+
+public class StudentTable {
+}
